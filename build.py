76d4dedcf40b79a3388a8d5a4ac1dcc5f32fcf97
Dan Korostelev <nadako@gmail.com>
2025-04-30 00:13:52 +0200

    also enable Vulkan backend

--- build.py
+++ build.py
@@ -24,7 +24,7 @@ git_heads = {
 # Note - tested with Odin version `dev-2025-01`
 
 # @CONFIGURE: Elements must be keys into below table
-wanted_backends = ["sdl3", "sdlgpu3"]
+wanted_backends = ["sdl3", "sdlgpu3", "vulkan"]
 # Supported means that an impl bindings file exists, and that it has been tested.
 # Some backends (like dx12, win32) have bindings but not been tested.
 backends = {
